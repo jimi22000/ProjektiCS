652d08ec1bbf4b711366feaec42a706a4ce777d4
Eemi100 <eemisalo@outlook.com>
2023-02-03 11:43:45 +0200

    Muutin alku näkymää

---
+++ ProjektiTiedosto/Projekti/UusiKayttaja.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekti
+{
+    class UusiKayttaja
+    {
+        public string nimi 
+        {
+
+        }
+
+        public string salasana 
+        {
+        
+        }
+
+        
+    }
+}
